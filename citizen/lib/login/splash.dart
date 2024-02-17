1783d4f2777dd7363a917805c66edde92a34e1e9
Kavya <guptakavya2002@gmail.com>
2024-02-17 11:28:15 +0530

    Add notification for Gov Alerts

--- citizen/lib/login/splash.dart
+++ citizen/lib/login/splash.dart
@@ -1,7 +1,8 @@
-import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'signin_page.dart';
 import '../../dbHelper/mongodb.dart';
+import 'package:socket_io_client/socket_io_client.dart' as io;
+import 'package:flutter_local_notifications/flutter_local_notifications.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key});
@@ -15,10 +16,67 @@ void main() {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  late io.Socket socket;
+  final FlutterLocalNotificationsPlugin flutterLocalNotificationsPlugin =
+  FlutterLocalNotificationsPlugin();
+
   @override
   void initState() {
     super.initState();
+
+    // Initialize the local notifications plugin
+    initializeNotifications();
+
+    // Connect to the WebSocket server
+    socket = io.io('https://node-server-us.onrender.com/', <String, dynamic>{
+      'transports': ['websocket'],
+      'autoConnect': true,
+    });
+
+    // Handle events from the server
+    socket.on('alertAdded', (data) {
+      // Handle the incoming data and show notifications
+      print('Received alertAdded event: $data');
+      showNotification(data.toString());
+    });
+
     _navigatetohome();
+  }
+
+  Future<void> initializeNotifications() async {
+    const AndroidInitializationSettings initializationSettingsAndroid =
+    AndroidInitializationSettings(
+        '@mipmap/ic_launcher'); // Update this line
+    const InitializationSettings initializationSettings =
+    InitializationSettings(android: initializationSettingsAndroid);
+
+    await flutterLocalNotificationsPlugin.initialize(
+      initializationSettings,
+      onSelectNotification: (String? payload) async {
+        // Handle notification click event
+      },
+    );
+  }
+
+  Future<void> showNotification(String message) async {
+    const AndroidNotificationDetails androidPlatformChannelSpecifics =
+    AndroidNotificationDetails(
+      'channel_id',
+      'channel_name',
+      importance: Importance.max,
+      priority: Priority.high,
+      showWhen: false,
+    );
+
+    const NotificationDetails platformChannelSpecifics =
+    NotificationDetails(android: androidPlatformChannelSpecifics);
+
+    await flutterLocalNotificationsPlugin.show(
+      0,
+      'New Alert',
+      message,
+      platformChannelSpecifics,
+    );
   }
 
   _navigatetohome() async {
--- citizen/lib/main.dart
+++ citizen/lib/main.dart
@@ -1,25 +1,46 @@
-import 'package:citizen/firebase_options.dart';
-import 'package:firebase_core/firebase_core.dart';
+// import 'package:citizen/firebase_options.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'login/splash.dart';
+// import 'package:flutter/material.dart';
+//
+// Future main() async {
+//   WidgetsFlutterBinding.ensureInitialized();
+//   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+//   runApp(
+//     const MyApp(),
+//   );
+// }
+//
+// class MyApp extends StatelessWidget {
+//   const MyApp({super.key});
+//   @override
+//   Widget build(BuildContext context) {
+//     return MaterialApp(
+//       theme: ThemeData(
+//         primaryColor: const Color(0xFF21222D),
+//       ),
+//       title: 'Urban Seva',
+//       home: const MyHomePage(),
+//       debugShowCheckedModeBanner: false,
+//     );
+//   }
+// }
+//
 
-import 'user_provider.dart';
-import 'package:provider/provider.dart';
-
-import 'login/splash.dart';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'login/splash.dart';
+import 'firebase_options.dart';
 
-Future main() async {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(
-    ChangeNotifierProvider(
-      create: (context) => UserProvider(),
-      child: const MyApp(),
-    ),
-  );
+  runApp(const MyApp());
 }
 
 class MyApp extends StatelessWidget {
-  const MyApp({super.key});
+  const MyApp({Key? key}) : super(key: key);
+
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
